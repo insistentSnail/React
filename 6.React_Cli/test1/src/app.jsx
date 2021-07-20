1128ef03882b9d0041ff5f31d787e377ffaff410
insistentSnail <1213736251@qq.com>
2021-07-20 23:43:10 +0800

    123

--- 6.React_Cli/test1/src/app.jsx
+++ 6.React_Cli/test1/src/app.jsx
@@ -1,27 +1,13 @@
 import React, { Component } from 'react'
-import { Button,DatePicker } from 'antd';
-// import 'antd/dist/antd.css' 全部样式
-import { WechatOutlined } from '@ant-design/icons';
-
-// 定义在类外面 onChange={onChange}这样调用
-// function onChange(date, dateString) {
-//   console.log(date, dateString);
-// }
+import Count from './components/Count'
 
 export default class App extends Component {
-
-  onChange = (date, dateString)=>{
-    console.log(date, dateString,1111);
-  }
 
   render() {
     return (
       <div>
         App...
-        <Button type="primary">Primary Button</Button>
-        <button>点我</button>
-        <WechatOutlined style={{ fontSize: '64px' }} />
-        <DatePicker onChange={this.onChange} />
+        <Count/>
       </div>
     )
   }
